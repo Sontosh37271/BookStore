60ddb68b8f1638929581fb62917da74e87980471
Sontosh37271 <sontoshshil37271@gmail.com>
2025-04-03 22:28:15 +0600

    backend and frontend connection is live now

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -11,7 +11,7 @@ const Freebook = () => {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstore-0ek3.onrender.com/book");
 
         const data = res.data.filter((data) => data.category === "Free");
         console.log(data);
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
           password: data.password,
         };
         await axios
-          .post("http://localhost:4001/user/login", userInfo)
+          .post("https://bookstore-0ek3.onrender.com/user/login", userInfo)
           .then((res) => {
             console.log(res.data);
             if (res.data) {
